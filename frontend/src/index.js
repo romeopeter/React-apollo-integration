3ac7a301b78b8bf99b0251228f71bb21e885fd12
Romeo-Peter <romeomeshack34@gmail.com>
2021-11-12 23:34:28 +0100

    Instiated Apollo client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,21 +7,21 @@ import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
-  inMemoryCache,
+  InMemoryCache,
 } from "@apollo/client";
 
 /*Instantiates Apollo client and connect to GraphQL server.
 This will take in the server link and an instantiation of the cache funtionalities*/
 const client = new ApolloClient({
-  link: createHttpLink({uri: "localhost:4000"}),
-  cache: new inMemoryCache()
+  link: createHttpLink({uri: "http://localhost:4000"}),
+  cache: new InMemoryCache()
 })
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  </ApolloProvider>
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
