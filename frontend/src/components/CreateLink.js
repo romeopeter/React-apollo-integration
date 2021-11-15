8680cec45c5a47021316d279c157335d038751f3
Romeo-Peter <romeomeshack34@gmail.com>
2021-11-15 21:55:06 +0100

    Added component to create link

---
+++ frontend/src/components/CreateLink.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+export default function CreateLink() {
+	const [formState, setFormState] = useState({ description: "", url: "" });
+
+	const CREATE_LINK_MUTATION = gql`
+		mutation PostMutation($url: String!, $description: String!) {
+			post(url: $url, description: $description) {
+				id
+				createdAt
+				url
+				description
+			}
+		}
+	`;
+
+	const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+		variables: {
+			description: formState.description,
+			url: formState.url,
+		},
+	});
+
+	return (
+		<div>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					createLink();
+				}}
+			>
+				<div className="flex flex-column mt3">
+					<input
+						type="text"
+						className="mt2"
+						type="text"
+						placeholder="A description for the link"
+						onChange={(e) =>
+							setFormState({
+								...formState,
+								description: e.target.value,
+							})
+						}
+						value={formState.description}
+					/>
+					<input
+						className="mb2"
+						value={formState.url}
+						onChange={(e) =>
+							setFormState({
+								...formState,
+								url: e.target.value,
+							})
+						}
+						type="text"
+						placeholder="The URL for the link"
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+}
