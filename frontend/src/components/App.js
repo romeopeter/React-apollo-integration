f09520febb112f24def9a9784592935079117218
Romeo-Peter <romeomeshack34@gmail.com>
2021-11-16 11:33:23 +0100

    Added react router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,21 @@
+import React from "react";
 import LinkList from "./LinkList";
-import '../styles/App.css';
+import CreateLink from "./CreateLink";
+import Header from "./Header";
+import { Routes, Route } from "react-router-dom";
+import "../styles/App.css";
 
 function App() {
   return (
-    <LinkList />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList/>} />
+          <Route path="/create" element={<CreateLink/>} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
