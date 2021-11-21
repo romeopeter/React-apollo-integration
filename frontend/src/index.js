fc52d47e72eefd535ad447d5be8538e5b58a78d0
Romeo-Peter <romeomeshack34@gmail.com>
2021-11-21 23:00:23 +0100

    Components entry file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import {
   ApolloProvider,
   ApolloClient,
@@ -8,11 +8,11 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import AUTH_TOKEN from "./constants";
+import {AUTH_TOKEN} from "./constants";
 import App from "./components/App";
 import "./styles/index.css";
 
-const httpLink = createHttpLink({ uri: "http://localhost:4000" });
+const httpLink = createHttpLink({ uri: "http://localhost:4000/", credentials: "same-origin" });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN);
@@ -24,19 +24,19 @@ const authLink = setContext((_, { headers }) => {
 });
 
 /*Instantiates Apollo client and connect to GraphQL server.
-This will take in the server link and an instantiation of the cache funtionalities*/
+This will take the server link and an instantiation of the cache funtionality*/
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <ApolloProvider client={client}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </ApolloProvider>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
