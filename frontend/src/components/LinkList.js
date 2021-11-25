39850351a910d6dccdad78c1360f9279b22f5432
Romeo-Peter <romeomeshack34@gmail.com>
2021-11-25 22:01:35 +0100

    Link list component

--- frontend/src/components/LinkList.js
+++ frontend/src/components/LinkList.js
@@ -2,44 +2,44 @@ import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import Link from "./Link";
 
-const LinkList = () => {
-  const linksToRender = [
-    {
-      id: "1",
-      description: "Prisma gives you a powerful database toolkit 😎",
-      url: "https://prisma.io",
-    },
-    {
-      id: "2",
-      description: "The best GraphQL client",
-      url: "https://www.apollographql.com/docs/react/",
-    },
-  ];
-
-  const FEED_QUERY = gql`
-    {
-      feed {
+export const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
         id
-        links {
+        createdAt
+        url
+        description
+        postedBy {
           id
-          createdAt
-          url
-          description
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
         }
       }
     }
-  `;
+  }
+`;
 
+const LinkList = () => {
   const { loading, error, data } = useQuery(FEED_QUERY);
 
-  if (loading) return <p style={{padding: 20, textAlign:"center"}}>Loading...</p>;
+  if (loading)
+    return <p style={{ padding: 20, textAlign: "center" }}>Loading...</p>;
 
   if (error) console.log(error);
 
   return (
     <div>
       {data &&
-        data.feed.links.map((link) => <Link key={link.id} link={link} />)}
+        data.feed.links.map((link, index) => (
+          <Link key={link.id} link={link} index={index} />
+        ))}
     </div>
   );
 };
